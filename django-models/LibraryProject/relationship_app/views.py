d938682d413e42c8b93b2770bf68efe8607f3195
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-08-12 23:57:15 +0300

    1. Django Views and URL Configuration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import DetailView
-from .models import Book, Author, Library, Librarian
+from .models import Book
+from .models import Library
 
 # Create your views here.
 
